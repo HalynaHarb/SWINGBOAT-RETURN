d32b47b4066f6b39f93a3721438498d94171dcc8
HalynaHarb <halyna.harbuzynska.sa.2019@lpnu.ua>
2020-05-11 23:10:53 +0300

    coins

--- SWINGBOAT/Assets/Scripts/ScoreManager.cs
+++ SWINGBOAT/Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
-    public TextMeshProGUI text;
+    public Text text;
     int score;
     
     // Start is called before the first frame update
