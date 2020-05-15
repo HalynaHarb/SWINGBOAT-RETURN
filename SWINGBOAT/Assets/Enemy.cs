15b14dce81d2ae7bf597e9ff9cf66ae5e7e9d268
HalynaHarb <halyna.harbuzynska.sa.2019@lpnu.ua>
2020-05-15 03:33:55 +0300

    EnemyMove + new Scene for Level1

--- SWINGBOAT/Assets/Enemy.cs
+++ SWINGBOAT/Assets/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public Animator animator;
     public int maxHealth = 100;
+    public bool MoveRight;
+    public float speed;
     int currentHealth;
     // Start is called before the first frame update
     void Start()
@@ -13,6 +15,34 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
     }
 
+    void Update () 
+    {
+        if(MoveRight) 
+        {
+            transform.Translate(2*Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2 (3,3);
+
+        }
+        else {
+            transform.Translate(-2*Time.deltaTime*speed, 0,0);
+            transform.localScale = new Vector2 (-3,3);
+        }
+ 
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("Turn"))
+        {
+            if(MoveRight){
+                MoveRight = false;
+            }
+            else 
+            {
+                MoveRight = true;
+            }
+        }
+    }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
