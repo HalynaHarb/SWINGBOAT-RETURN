bbee9d2b0ee3e4f146abc5d33f1bf600e8a23f87
jeanixys <jujiys@gmail.com>
2020-05-19 20:51:37 +0300

    Menu design

---
+++ SWINGBOAT/Assets/SartTutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SartTutorial : MonoBehaviour
+{
+   public void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+    }
+}
