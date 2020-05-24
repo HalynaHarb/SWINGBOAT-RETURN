267994f0b1bd7a1ef0e94984acc1dc2bd6f0b787
HalynaHarb <halyna.harbuzynska.sa.2019@lpnu.ua>
2020-05-24 22:05:58 +0300

    New

---
+++ SWINGBOAT/Assets/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight : Enemy
+{
+    protected Animator animator;
+
+    protected virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void JumpedOn()
+    {
+        animator.SetTrigger("Death");
+    }
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
