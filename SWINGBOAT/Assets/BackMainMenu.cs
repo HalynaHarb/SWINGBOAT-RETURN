a1adb11bd948a88ebcba29de938072a24ccbb930
jeanixys <jujiys@gmail.com>
2020-06-03 19:25:32 +0300

    poop

--- SWINGBOAT/Assets/BackMainMenu.cs
+++ SWINGBOAT/Assets/BackMainMenu.cs
@@ -5,12 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class BackMainMenu : MonoBehaviour
 {
+    private int nextScene;
+
+    private void Start()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
     public void BackToMainMenu()
     {
         SceneManager.LoadScene(1);
     }
     public void NextLevel()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(nextScene);
     }
 }
